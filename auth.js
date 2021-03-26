dae3691d7f33ae5c02529ffab58f3601571d7299
Jasprit Kaur <jasprit.kaur.2908@gmail.com>
2021-03-26 20:44:35 +0530

    first commit

---
+++ auth.js
@@ -0,0 +1,56 @@
+// get elements for authentication
+const SuName = document.getElementById("txtNameSU");
+const SuEmail = document.getElementById("txtEmailSU");
+const SuPassword = document.getElementById("txtPasswordSU");
+
+const SiEmail = document.getElementById("txtEmailSI");
+const SiPassword = document.getElementById("txtPasswordSI");
+
+const btnSignup = document.getElementById("btnSignup");
+const btnSignin = document.getElementById("btnSignin");
+
+const signupForm = document.getElementById('sign-up-container');
+const signinForm = document.getElementById('sign-in-container');
+
+function closeModal(){
+        modal.style.display = 'none';
+}
+
+// Sign Up
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = signupForm['txtNameSU'].value;
+    const email = signupForm['txtEmailSU'].value;
+    const password = signupForm['txtPasswordSU'].value;
+    console.log(name,email,password);
+    // signup the user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {console.log(cred)})
+    alert("You are registered :)");
+})
+btnSignup.addEventListener('click', closeModal);
+
+// Sign In
+signinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = signinForm['txtEmailSI'].value;
+    const password = signinForm['txtPasswordSI'].value;
+    console.log(email,password);
+    // signup the user
+    auth.signInWithEmailAndPassword(email, password).then(cred => {console.log(cred)})
+    alert("Thank your logged in :)");
+})
+btnSignin.addEventListener('click', closeModal);
+
+
+// Google Auth
+
+googleSignIn=() =>{
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("Success");
+  }).catch((error) => {
+    console.log(error);
+    console.log("Failed");
+  });
+}
